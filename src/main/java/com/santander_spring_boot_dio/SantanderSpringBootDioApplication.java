f77a42add83edd7934fc79d9d74903127c1003e8
jandin88 <jandersson.coutinho@gmail.com>
2023-10-06 10:58:11 -0300

    initial commit

---
+++ src/main/java/com/santander_spring_boot_dio/SantanderSpringBootDioApplication.java
@@ -0,0 +1,13 @@
+package com.santander_spring_boot_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderSpringBootDioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SantanderSpringBootDioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/santander_spring_boot_dio/SantanderSpringBootDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander_spring_boot_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderSpringBootDioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
